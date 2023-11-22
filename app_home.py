bcab9b43ecf9e51069a72644595f6437672fc48c
Ray <proayoung2@gmail.com>
2023-11-22 17:01:44 +0900

    20231122-2

--- app_home.py
+++ app_home.py
@@ -5,4 +5,6 @@ def run_home_app() :
     st.text("이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.")
     st.text("고객 정보를 넣으면, 차 구매 금액도 예측해 줍니다.")
 
+    st.text('AWS에 배포까지 된 앱입니다.')
+    
     st.image("https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2021%2F06%2FCadillac-launch-new-escalade-in-korea-luxury-suv-2.jpg?cbr=1&q=90")
