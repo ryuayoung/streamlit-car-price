4abc396203047695a3889d96d330e8308f4e5da0
Ray <proayoung2@gmail.com>
2023-11-22 15:21:14 +0900

    20231122

--- app_home.py
+++ app_home.py
@@ -5,4 +5,4 @@ def run_home_app() :
     st.text("이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.")
     st.text("고객 정보를 넣으면, 차 구매 금액도 예측해 줍니다.")
 
-    st.image("https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,h_699,q_auto:eco,w_1242/v1/vehicles/used/bmw/3-series/2023/3487-bmd16044-1")
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsd9Mv8rBwzy9JSCJZQOalDCipiaUYnfRteA&usqp=CAU", use_column_width=True)
